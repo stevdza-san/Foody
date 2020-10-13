1465a08f2dce42f79e2f1b706e9daac6638a4373
Stefan <stefan.jovanavich@gmail.com>
2020-10-13 12:05:37 +0200

    placeholder_row_layout.xml designed

--- app/src/main/java/com/example/foody/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/RecipesFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_recipes.*
+import kotlinx.android.synthetic.main.fragment_recipes.view.*
 
 class RecipesFragment : Fragment() {
 
@@ -13,7 +15,9 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        return view
     }
 
 }
