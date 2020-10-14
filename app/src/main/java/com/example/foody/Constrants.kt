7ab4d9336cf46263a45c74fbcad55a9b232cd22a
Stefan <stefan.jovanavich@gmail.com>
2020-10-14 10:12:28 +0200

    Constants class created

---
+++ app/src/main/java/com/example/foody/Constrants.kt
@@ -0,0 +1,12 @@
+package com.example.foody
+
+class Constrants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "c4a65a4f55af4b17ac4b13ef7163b71d"
+
+    }
+
+}
