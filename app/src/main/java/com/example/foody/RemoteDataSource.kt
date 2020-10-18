f378c04084d4563a8461e2b30877f921b31bad47
Stefan <stefan.jovanavich@gmail.com>
2020-10-18 09:04:05 +0200

    RemoteDataSource.kt created

--- app/src/main/java/com/example/foody/RemoteDataSource.kt
+++ app/src/main/java/com/example/foody/RemoteDataSource.kt
@@ -1,4 +1,15 @@
 package com.example.foody
 
-class RemoteDataSource() {
+import com.example.foody.models.FoodRecipe
+import retrofit2.Response
+import javax.inject.Inject
+
+class RemoteDataSource @Inject constructor(
+    private val foodRecipesApi: FoodRecipesApi
+) {
+
+    suspend fun getRecipes(queries: Map<String, String>): Response<FoodRecipe> {
+        return foodRecipesApi.getRecipes(queries)
+    }
+
 }
