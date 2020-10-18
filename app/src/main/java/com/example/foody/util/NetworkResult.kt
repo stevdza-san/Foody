b541d09d47b4b95bae2713254115c00915c54e65
Stefan <stefan.jovanavich@gmail.com>
2020-10-18 09:21:59 +0200

    NetworkResult.kt created

---
+++ app/src/main/java/com/example/foody/util/NetworkResult.kt
@@ -0,0 +1,12 @@
+package com.example.foody.util
+
+sealed class NetworkResult<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+
+    class Success<T>(data: T): NetworkResult<T>(data)
+    class Error<T>(message: String?, data: T? = null): NetworkResult<T>(data, message)
+    class Loading<T>: NetworkResult<T>()
+
+}
