8f7a05c09f90c159dfb81f3bd87763bb6eb28049
Stefan <stefan.jovanavich@gmail.com>
2020-10-22 15:33:41 +0200

    RecipesDiffUtil.kt created

--- app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
+++ app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
@@ -2,10 +2,12 @@ package com.example.foody.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.example.foody.databinding.RecipesRowLayoutBinding
 import com.example.foody.models.FoodRecipe
 import com.example.foody.models.Result
+import com.example.foody.util.RecipesDiffUtil
 
 class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
 
@@ -43,7 +45,10 @@ class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
     }
 
     fun setData(newData: FoodRecipe){
+        val recipesDiffUtil =
+            RecipesDiffUtil(recipe, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
         recipe = newData.results
-        notifyDataSetChanged()
+        diffUtilResult.dispatchUpdatesTo(this)
     }
 }
---
+++ app/src/main/java/com/example/foody/util/RecipesDiffUtil.kt
@@ -0,0 +1,25 @@
+package com.example.foody.util
+
+import androidx.recyclerview.widget.DiffUtil
+import com.example.foody.models.Result
+
+class RecipesDiffUtil(
+    private val oldList: List<Result>,
+    private val newList: List<Result>
+): DiffUtil.Callback() {
+    override fun getOldListSize(): Int {
+        return oldList.size
+    }
+
+    override fun getNewListSize(): Int {
+        return newList.size
+    }
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] === newList[newItemPosition]
+    }
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int): Boolean {
+        return oldList[oldItemPosition] == newList[newItemPosition]
+    }
+}
