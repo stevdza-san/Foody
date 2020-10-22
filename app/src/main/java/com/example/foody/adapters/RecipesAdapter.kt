3fd9b04030f68120a537282b7bd0f5e626b0622c
Stefan <stefan.jovanavich@gmail.com>
2020-10-22 12:04:49 +0200

    RecipesAdapter.kt created

---
+++ app/src/main/java/com/example/foody/adapters/RecipesAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.foody.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foody.databinding.RecipesRowLayoutBinding
+import com.example.foody.models.FoodRecipe
+import com.example.foody.models.Result
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipe = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(result: Result){
+            binding.result = result
+            binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
+
+    fun setData(newData: FoodRecipe){
+        recipe = newData.results
+        notifyDataSetChanged()
+    }
+}
