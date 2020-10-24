0136ce985ad1b2cd28f9b8c17bbc3b72d46390e4
Stefan <stefan.jovanavich@gmail.com>
2020-10-24 13:08:06 +0200

    RecipesDao.kt created

---
+++ app/src/main/java/com/example/foody/RecipesDao.kt
@@ -0,0 +1,18 @@
+package com.example.foody
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipes(recipesEntity: RecipesEntity)
+
+    @Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    fun readRecipes(): Flow<List<RecipesEntity>>
+
+}
