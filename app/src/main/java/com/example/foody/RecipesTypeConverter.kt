82def6a2b77bd035a39aa063cf1a1243eabc131a
Stefan <stefan.jovanavich@gmail.com>
2020-10-24 13:14:51 +0200

    RecipesTypeConverter.kt created

---
+++ app/src/main/java/com/example/foody/RecipesTypeConverter.kt
@@ -0,0 +1,23 @@
+package com.example.foody
+
+import androidx.room.TypeConverter
+import com.example.foody.models.FoodRecipe
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+
+class RecipesTypeConverter {
+
+    var gson = Gson()
+
+    @TypeConverter
+    fun foodRecipeToString(foodRecipe: FoodRecipe): String {
+        return gson.toJson(foodRecipe)
+    }
+
+    @TypeConverter
+    fun stringToFoodRecipe(data: String): FoodRecipe {
+        val listType = object : TypeToken<FoodRecipe>() {}.type
+        return gson.fromJson(data, listType)
+    }
+
+}
