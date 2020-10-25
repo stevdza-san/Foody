8bf8ae05c73c4d6e2b8065088bc674e706a6794c
Stefan <stefan.jovanavich@gmail.com>
2020-10-25 08:29:47 +0100

    RecipesDatabase.kt created

--- app/src/main/java/com/example/foody/RecipesDatabase.kt
+++ app/src/main/java/com/example/foody/RecipesDatabase.kt
@@ -2,12 +2,14 @@ package com.example.foody
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 
 @Database(
     entities = [RecipesEntity::class],
     version = 1,
     exportSchema = false
 )
+@TypeConverters(RecipesTypeConverter::class)
 abstract class RecipesDatabase: RoomDatabase() {
 
     abstract fun recipesDao(): RecipesDao
