06c5497da7457676853e6e60279158bb9ff09b97
Stefan <stefan.jovanavich@gmail.com>
2020-10-25 08:42:52 +0100

    DatabaseModule.kt created

---
+++ app/src/main/java/com/example/foody/di/DatabaseModule.kt
@@ -0,0 +1,32 @@
+package com.example.foody.di
+
+import android.content.Context
+import androidx.room.Room
+import com.example.foody.data.database.RecipesDatabase
+import com.example.foody.util.Constants.Companion.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(
+        context,
+        RecipesDatabase::class.java,
+        DATABASE_NAME
+    ).build()
+
+    @Singleton
+    @Provides
+    fun provideDao(database: RecipesDatabase) = database.recipesDao()
+
+}
