b653940b390597ac124a63e45ea0f02a25780155
Stefan <stefan.jovanavich@gmail.com>
2020-10-26 17:22:44 +0100

    RecipesBinding.kt created

---
+++ app/src/main/java/com/example/foody/bindingadapters/RecipesBinding.kt
@@ -0,0 +1,50 @@
+package com.example.foody.bindingadapters
+
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.foody.data.database.RecipesEntity
+import com.example.foody.models.FoodRecipe
+import com.example.foody.util.NetworkResult
+
+class RecipesBinding {
+
+    companion object {
+
+        @BindingAdapter("readApiResponse", "readDatabase", requireAll = true)
+        @JvmStatic
+        fun errorImageViewVisibility(
+            imageView: ImageView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                imageView.visibility = View.VISIBLE
+            } else if (apiResponse is NetworkResult.Loading) {
+                imageView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                imageView.visibility = View.INVISIBLE
+            }
+        }
+
+        @BindingAdapter("readApiResponse2", "readDatabase2", requireAll = true)
+        @JvmStatic
+        fun errorTextViewVisibility(
+            textView: TextView,
+            apiResponse: NetworkResult<FoodRecipe>?,
+            database: List<RecipesEntity>?
+        ) {
+            if (apiResponse is NetworkResult.Error && database.isNullOrEmpty()) {
+                textView.visibility = View.VISIBLE
+                textView.text = apiResponse.message.toString()
+            } else if (apiResponse is NetworkResult.Loading) {
+                textView.visibility = View.INVISIBLE
+            } else if (apiResponse is NetworkResult.Success) {
+                textView.visibility = View.INVISIBLE
+            }
+        }
+
+    }
+
+}
--- app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/recipes/RecipesFragment.kt
@@ -13,6 +13,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.foody.viewmodels.MainViewModel
 import com.example.foody.R
 import com.example.foody.adapters.RecipesAdapter
+import com.example.foody.databinding.FragmentRecipesBinding
 import com.example.foody.util.NetworkResult
 import com.example.foody.util.observeOnce
 import com.example.foody.viewmodels.RecipesViewModel
@@ -23,10 +24,12 @@ import kotlinx.coroutines.launch
 @AndroidEntryPoint
 class RecipesFragment : Fragment() {
 
+    private var _binding: FragmentRecipesBinding? = null
+    private val binding get() = _binding!!
+
     private lateinit var mainViewModel: MainViewModel
     private lateinit var recipesViewModel: RecipesViewModel
     private val mAdapter by lazy { RecipesAdapter() }
-    private lateinit var mView: View
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,17 +42,19 @@ class RecipesFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        mView = inflater.inflate(R.layout.fragment_recipes, container, false)
+        _binding = FragmentRecipesBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+        binding.mainViewModel = mainViewModel
 
         setupRecyclerView()
         readDatabase()
 
-        return mView
+        return binding.root
     }
 
     private fun setupRecyclerView() {
-        mView.recyclerview.adapter = mAdapter
-        mView.recyclerview.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerview.adapter = mAdapter
+        binding.recyclerview.layoutManager = LinearLayoutManager(requireContext())
         showShimmerEffect()
     }
 
@@ -103,11 +108,15 @@ class RecipesFragment : Fragment() {
     }
 
     private fun showShimmerEffect() {
-        mView.recyclerview.showShimmer()
+        binding.recyclerview.showShimmer()
     }
 
     private fun hideShimmerEffect() {
-        mView.recyclerview.hideShimmer()
+        binding.recyclerview.hideShimmer()
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 }
