1789069cdd0f72e95953b5d0edb28bd220f7609b
Stefan <stefan.jovanavich@gmail.com>
2020-10-31 10:22:59 +0100

    FoodRecipesApi.kt modified

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -12,4 +12,9 @@ interface FoodRecipesApi {
         @QueryMap queries: Map<String, String>
     ): Response<FoodRecipe>
 
+    @GET("/recipes/complexSearch")
+    suspend fun searchRecipes(
+        @QueryMap searchQuery: Map<String, String>
+    ): Response<FoodRecipe>
+
 }
