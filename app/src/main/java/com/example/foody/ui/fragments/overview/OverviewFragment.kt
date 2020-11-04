c02acbe62e2bba70d0e0d1ae8c4c8721fa321c26
Stefan <stefan.jovanavich@gmail.com>
2020-11-04 10:54:06 +0100

    Display Data in OverviewFragment.kt

--- app/src/main/java/com/example/foody/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/example/foody/ui/fragments/overview/OverviewFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import coil.load
 import com.example.foody.R
+import com.example.foody.models.Result
+import kotlinx.android.synthetic.main.fragment_overview.view.*
 
 class OverviewFragment : Fragment() {
 
@@ -14,7 +18,48 @@ class OverviewFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_overview, container, false)
+        val view = inflater.inflate(R.layout.fragment_overview, container, false)
+
+        val args = arguments
+        val myBundle: Result? = args?.getParcelable("recipeBundle")
+
+        view.main_imageView.load(myBundle?.image)
+        view.title_textView.text = myBundle?.title
+        view.likes_textView.text = myBundle?.aggregateLikes.toString()
+        view.time_textView.text = myBundle?.readyInMinutes.toString()
+        view.summary_textView.text = myBundle?.summary
+
+        if(myBundle?.vegetarian == true){
+            view.vegetarian_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.vegetarian_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.vegan == true){
+            view.vegan_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.vegan_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.glutenFree == true){
+            view.gluten_free_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.gluten_free_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.dairyFree == true){
+            view.dairy_free_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.dairy_free_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.veryHealthy == true){
+            view.healthy_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.healthy_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        if(myBundle?.cheap == true){
+            view.cheap_imageView.setColorFilter(ContextCompat.getColor(requireContext(), R.color.green))
+            view.cheap_textView.setTextColor(ContextCompat.getColor(requireContext(), R.color.green))
+        }
+
+        return view
     }
 
 }
