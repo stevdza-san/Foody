8cc016063938213e98eabfcdfa2f9e0a89b81667
Stefan <stefan.jovanavich@gmail.com>
2020-11-06 11:20:44 +0100

    RecipesDiffUtil.kt modified

--- app/src/main/java/com/example/foody/util/RecipesDiffUtil.kt
+++ app/src/main/java/com/example/foody/util/RecipesDiffUtil.kt
@@ -3,9 +3,9 @@ package com.example.foody.util
 import androidx.recyclerview.widget.DiffUtil
 import com.example.foody.models.Result
 
-class RecipesDiffUtil(
-    private val oldList: List<Result>,
-    private val newList: List<Result>
+class RecipesDiffUtil<T>(
+    private val oldList: List<T>,
+    private val newList: List<T>
 ): DiffUtil.Callback() {
     override fun getOldListSize(): Int {
         return oldList.size
