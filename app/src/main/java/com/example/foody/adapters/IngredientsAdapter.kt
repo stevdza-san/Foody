f78d2dbf80286bf28d1cd22d5826995d995fa668
Stefan <stefan.jovanavich@gmail.com>
2020-11-06 11:47:11 +0100

    IngredientsAdapter.kt created

---
+++ app/src/main/java/com/example/foody/adapters/IngredientsAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.foody.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import com.example.foody.R
+import com.example.foody.models.ExtendedIngredient
+import com.example.foody.util.Constants.Companion.BASE_IMAGE_URL
+import com.example.foody.util.RecipesDiffUtil
+import kotlinx.android.synthetic.main.ingredients_row_layout.view.*
+import java.util.*
+
+class IngredientsAdapter: RecyclerView.Adapter<IngredientsAdapter.MyViewHolder>() {
+
+    private var ingredientsList = emptyList<ExtendedIngredient>()
+
+    class MyViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.ingredients_row_layout, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.itemView.ingredient_imageView.load(BASE_IMAGE_URL + ingredientsList[position].image)
+        holder.itemView.ingredient_name.text = ingredientsList[position].name.capitalize(Locale.ROOT)
+        holder.itemView.ingredient_amount.text = ingredientsList[position].amount.toString()
+        holder.itemView.ingredient_unit.text = ingredientsList[position].unit
+        holder.itemView.ingredient_consistency.text = ingredientsList[position].consistency
+        holder.itemView.ingredient_original.text = ingredientsList[position].original
+    }
+
+    override fun getItemCount(): Int {
+        return ingredientsList.size
+    }
+
+    fun setData(newIngredients: List<ExtendedIngredient>) {
+        val ingredientsDiffUtil =
+            RecipesDiffUtil(ingredientsList, newIngredients)
+        val diffUtilResult = DiffUtil.calculateDiff(ingredientsDiffUtil)
+        ingredientsList = newIngredients
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+
+}
--- app/src/main/java/com/example/foody/util/Constants.kt
+++ app/src/main/java/com/example/foody/util/Constants.kt
@@ -5,6 +5,7 @@ class Constants {
     companion object {
 
         const val BASE_URL = "https://api.spoonacular.com"
+        const val BASE_IMAGE_URL = "https://spoonacular.com/cdn/ingredients_100x100/"
         const val API_KEY = "[YOUR API KEY HERE]"
 
         // API Query Keys
