6c333e917f66a26a968960915e6ef0de2e6e981e
Stefan <stefan.jovanavich@gmail.com>
2020-11-09 11:53:51 +0100

    RecipesDatabase.kt modified

--- app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
+++ app/src/main/java/com/example/foody/data/database/RecipesDatabase.kt
@@ -3,10 +3,11 @@ package com.example.foody.data.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import com.example.foody.data.database.entities.FavoritesEntity
 import com.example.foody.data.database.entities.RecipesEntity
 
 @Database(
-    entities = [RecipesEntity::class],
+    entities = [RecipesEntity::class, FavoritesEntity::class],
     version = 1,
     exportSchema = false
 )
