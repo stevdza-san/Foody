761ec3e8cccdc9712e340a3889b1f7891b7f2611
Stefan <stefan.jovanavich@gmail.com>
2020-11-14 13:13:29 +0100

    RemoteDataSource.kt modified

--- app/src/main/java/com/example/foody/data/RemoteDataSource.kt
+++ app/src/main/java/com/example/foody/data/RemoteDataSource.kt
@@ -1,6 +1,7 @@
 package com.example.foody.data
 
 import com.example.foody.data.network.FoodRecipesApi
+import com.example.foody.models.FoodJoke
 import com.example.foody.models.FoodRecipe
 import retrofit2.Response
 import javax.inject.Inject
@@ -15,6 +16,10 @@ class RemoteDataSource @Inject constructor(
 
     suspend fun searchRecipes(searchQuery: Map<String, String>): Response<FoodRecipe> {
         return foodRecipesApi.searchRecipes(searchQuery)
+    }
+
+    suspend fun getFoodJoke(apiKey: String): Response<FoodJoke> {
+        return foodRecipesApi.getFoodJoke(apiKey)
     }
 
 }
