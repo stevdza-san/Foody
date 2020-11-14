5a04a04d7710057fd94806b067b592edbe994542
Stefan <stefan.jovanavich@gmail.com>
2020-11-14 13:10:19 +0100

    FoodRecipesApi.kt modified

--- app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
+++ app/src/main/java/com/example/foody/data/network/FoodRecipesApi.kt
@@ -1,8 +1,10 @@
 package com.example.foody.data.network
 
+import com.example.foody.models.FoodJoke
 import com.example.foody.models.FoodRecipe
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 import retrofit2.http.QueryMap
 
 interface FoodRecipesApi {
@@ -16,5 +18,10 @@ interface FoodRecipesApi {
     suspend fun searchRecipes(
         @QueryMap searchQuery: Map<String, String>
     ): Response<FoodRecipe>
+
+    @GET("food/jokes/random")
+    suspend fun getFoodJoke(
+        @Query("apiKey") apiKey: String
+    ): Response<FoodJoke>
 
 }
