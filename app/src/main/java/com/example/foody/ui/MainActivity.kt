b8d89ac840419bef2dae46ecb7c5b54c35b2a0b5
Stefan <stefan.jovanavich@gmail.com>
2020-11-17 19:29:22 +0100

    Splash Screen Added

--- app/src/main/java/com/example/foody/ui/MainActivity.kt
+++ app/src/main/java/com/example/foody/ui/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.AppTheme)
         setContentView(R.layout.activity_main)
 
         navController = findNavController(R.id.navHostFragment)
