5d1b801abf1a2b9663d993aae3f2631066cd4be0
Stefan <stefan.jovanavich@gmail.com>
2020-11-24 17:26:17 +0100

    DataStore 1.0.0-alpha04 - Package Error Fixed

--- app/src/main/java/com/example/foody/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/foody/data/DataStoreRepository.kt
@@ -1,8 +1,12 @@
 package com.example.foody.data
 
 import android.content.Context
-import androidx.datastore.DataStore
+import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.*
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.core.preferencesKey
 import com.example.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
 import com.example.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.foody.util.Constants.Companion.PREFERENCES_BACK_ONLINE
