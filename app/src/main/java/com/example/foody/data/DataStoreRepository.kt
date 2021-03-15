e2fe485d5eaf1cd5fbe90c03d932e3600f5cbee8
Stefan <stefan.jovanavich@gmail.com>
2021-03-15 17:13:35 +0100

    Dependency Update - DataStore-Alpha08

--- app/src/main/java/com/example/foody/data/DataStoreRepository.kt
+++ app/src/main/java/com/example/foody/data/DataStoreRepository.kt
@@ -14,13 +14,16 @@ import com.example.foody.util.Constants.Companion.PREFERENCES_MEAL_TYPE_ID
 import com.example.foody.util.Constants.Companion.PREFERENCES_NAME
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ActivityRetainedScoped
+import dagger.hilt.android.scopes.ViewModelScoped
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.map
 import java.io.IOException
 import javax.inject.Inject
 
-@ActivityRetainedScoped
+private val Context.dataStore by preferencesDataStore(PREFERENCES_NAME)
+
+@ViewModelScoped
 class DataStoreRepository @Inject constructor(@ApplicationContext private val context: Context) {
 
     private object PreferenceKeys {
@@ -31,9 +34,7 @@ class DataStoreRepository @Inject constructor(@ApplicationContext private val co
         val backOnline = booleanPreferencesKey(PREFERENCES_BACK_ONLINE)
     }
 
-    private val dataStore: DataStore<Preferences> = context.createDataStore(
-        name = PREFERENCES_NAME
-    )
+    private val dataStore: DataStore<Preferences> = context.dataStore
 
     suspend fun saveMealAndDietType(
         mealType: String,
