4467b38ada69fcdb64c100d4b9b267c67570bab8
Stefan <stefan.jovanavich@gmail.com>
2021-06-12 11:03:47 +0200

    Fixed bug associated with mealAndDiet variable from RecipesViewModel.kt

--- app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
+++ app/src/main/java/com/example/foody/viewmodels/RecipesViewModel.kt
@@ -8,6 +8,8 @@ import androidx.lifecycle.viewModelScope
 import com.example.foody.data.DataStoreRepository
 import com.example.foody.data.MealAndDietType
 import com.example.foody.util.Constants.Companion.API_KEY
+import com.example.foody.util.Constants.Companion.DEFAULT_DIET_TYPE
+import com.example.foody.util.Constants.Companion.DEFAULT_MEAL_TYPE
 import com.example.foody.util.Constants.Companion.DEFAULT_RECIPES_NUMBER
 import com.example.foody.util.Constants.Companion.QUERY_ADD_RECIPE_INFORMATION
 import com.example.foody.util.Constants.Companion.QUERY_API_KEY
@@ -37,12 +39,14 @@ class RecipesViewModel @Inject constructor(
 
     fun saveMealAndDietType() =
         viewModelScope.launch(Dispatchers.IO) {
-            dataStoreRepository.saveMealAndDietType(
-                mealAndDiet.selectedMealType,
-                mealAndDiet.selectedMealTypeId,
-                mealAndDiet.selectedDietType,
-                mealAndDiet.selectedDietTypeId
-            )
+            if (this@RecipesViewModel::mealAndDiet.isInitialized) {
+                dataStoreRepository.saveMealAndDietType(
+                    mealAndDiet.selectedMealType,
+                    mealAndDiet.selectedMealTypeId,
+                    mealAndDiet.selectedDietType,
+                    mealAndDiet.selectedDietTypeId
+                )
+            }
         }
 
     fun saveMealAndDietTypeTemp(
@@ -69,10 +73,16 @@ class RecipesViewModel @Inject constructor(
 
         queries[QUERY_NUMBER] = DEFAULT_RECIPES_NUMBER
         queries[QUERY_API_KEY] = API_KEY
-        queries[QUERY_TYPE] = mealAndDiet.selectedMealType
-        queries[QUERY_DIET] = mealAndDiet.selectedDietType
         queries[QUERY_ADD_RECIPE_INFORMATION] = "true"
         queries[QUERY_FILL_INGREDIENTS] = "true"
+
+        if (this@RecipesViewModel::mealAndDiet.isInitialized) {
+            queries[QUERY_TYPE] = mealAndDiet.selectedMealType
+            queries[QUERY_DIET] = mealAndDiet.selectedDietType
+        } else {
+            queries[QUERY_TYPE] = DEFAULT_MEAL_TYPE
+            queries[QUERY_DIET] = DEFAULT_DIET_TYPE
+        }
 
         return queries
     }
