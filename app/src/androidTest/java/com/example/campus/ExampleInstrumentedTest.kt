0aa2e7f9c8d5d52d28ebbc5ea23960e3c8838dfe
Fengnan <soniasun0925@gmail.com>
2023-02-20 19:12:49 -0800

    Set up App Navigation.

--- app/src/androidTest/java/com/example/campus/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/campus/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.example.foody
+package com.example.campus
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
---
+++ app/src/main/java/com/example/campus/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.campus
+
+class Constants {
+    companion object {
+
+        const val BASE_URL = "https://api"
+        const val API_KEY = "[YOUR API KEY HERE]"
+
+    }
+}
